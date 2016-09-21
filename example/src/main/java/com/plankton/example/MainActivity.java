3186c1fa461bf4bae673178e1159ac383cca2700
Pongtep Pakakat <Mercury@Pongteps-MacBook-Air.local>
2016-09-22 00:19:20 +0700

    create canvas and save bitmap

--- example/src/main/java/com/plankton/example/MainActivity.java
+++ example/src/main/java/com/plankton/example/MainActivity.java
@@ -1,14 +1,178 @@
 package com.plankton.example;
 
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.plankton.design.SignatureView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.UUID;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private SignatureView signatureView;
+    private static final int REQUEST_WRITE_STORAGE = 112;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        signatureView = (SignatureView) findViewById(R.id.signatureView);
+
+        boolean hasPermission = (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermission) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_STORAGE);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.activity_main_menu, menu);
+        return true;
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.delete:
+                new AlertDialog.Builder(this)
+                        .setTitle("Confirm ?")
+                        .setMessage("Clear canvas.")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                signatureView.clearCanvas();
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null).show();
+
+                return true;
+            case R.id.save:
+                new AlertDialog.Builder(this)
+                        .setTitle("Confirm ?")
+                        .setMessage("Save to disk.")
+                        .setIcon(android.R.drawable.ic_dialog_alert)
+                        .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+
+                            public void onClick(DialogInterface dialog, int whichButton) {
+
+
+
+                                saveImage();
+
+
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, null).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    // save image to sdcard and seng broadcast to re-mount gallery.
+
+    private void saveImage() {
+        Bitmap sign = signatureView.getmCanvasBitmap();
+
+//        File myDir = new File(Environment.getExternalStorageDirectory(
+//                Environment.DIRECTORY_PICTURES), getResources().getString(R.string.imageFolderName));
+//        if (!myDir.mkdirs()) {
+//            Log.e("LOG_TAG", "Directory not created");
+//        }
+
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/" + getResources().getString(R.string.imageFolderName));
+        if (!myDir.mkdirs()) {
+            Log.e("LOG_TAG", "Directory not created");
+        }
+
+        String fname = UUID.randomUUID().toString() + ".jpg";
+        File file = new File(myDir, fname);
+
+        if (file.exists()) {
+            file.delete();
+        }
+
+
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+
+            sign.compress(Bitmap.CompressFormat.JPEG, 90, out);
+
+            out.flush();
+            out.close();
+
+
+            // update photo gallery.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                Intent mediaScanIntent = new Intent(
+                        Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+                Uri contentUri = Uri.fromFile(file);
+                mediaScanIntent.setData(contentUri);
+                this.sendBroadcast(mediaScanIntent);
+            } else {
+                sendBroadcast(new Intent(
+                        Intent.ACTION_MEDIA_MOUNTED,
+                        Uri.parse("file://"
+                                + Environment.getExternalStorageDirectory())));
+            }
+
+            Toast.makeText(this, "SUCCESS : " + fname, Toast.LENGTH_LONG).show();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode)
+        {
+            case REQUEST_WRITE_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    //reload my activity with permission granted or use the features what required the permission
+                } else
+                {
+                    Toast.makeText(getParent(), "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+
     }
 }
--- signatureview/src/main/java/com/plankton/design/SignatureView.java
+++ signatureview/src/main/java/com/plankton/design/SignatureView.java
@@ -4,8 +4,10 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -16,16 +18,38 @@ import android.view.View;
 
 public class SignatureView extends View {
 
-    //drawing path
-    private Path drawPath;
-    //drawing and canvas paint
-    private Paint drawPaint, canvasPaint;
-    //initial color
-    private int paintColor = 0xFF660000;
-    //canvas
-    private Canvas drawCanvas;
-    //canvas bitmap
-    private Bitmap canvasBitmap;
+
+    private boolean mIsReadOnly = false;
+    private float mStrokeWidth = 5;
+    private int mPaintColor = Color.BLACK;
+    private int mBackgroundColor = Color.WHITE;
+    private int mLastHeight;
+    private int mLastWidth;
+
+    private Bitmap mCanvasBitmap;
+
+
+    private Path mDrawPath;
+    private Paint mDrawPaint, mCanvasPaint;
+
+    private Canvas mDrawCanvas;
+
+    public Bitmap getmCanvasBitmap() {
+        return mCanvasBitmap;
+    }
+
+    public void setmCanvasBitmap(Bitmap mCanvasBitmap) {
+        this.mCanvasBitmap = mCanvasBitmap;
+    }
+
+
+    public void setmStrokeWidth(float mStrokeWidth) {
+        this.mStrokeWidth = mStrokeWidth;
+    }
+
+    public void setmPaintColor(int mPaintColor) {
+        this.mPaintColor = mPaintColor;
+    }
 
     public SignatureView(Context context) {
         super(context);
@@ -52,57 +76,81 @@ public class SignatureView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        canvas.drawBitmap(canvasBitmap, 0, 0, canvasPaint);
-        canvas.drawPath(drawPath, drawPaint);
+        canvas.drawBitmap(mCanvasBitmap, 0, 0, mCanvasPaint);
+        canvas.drawPath(mDrawPath, mDrawPaint);
     }
 
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
+        mLastHeight = h;
+        mLastWidth = w;
 
-        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
-        drawCanvas = new Canvas(canvasBitmap);
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        mCanvasBitmap.eraseColor(mBackgroundColor);
+        mDrawCanvas = new Canvas();
+
+        mDrawCanvas.drawColor(mBackgroundColor, PorterDuff.Mode.CLEAR);
+        mDrawCanvas.setBitmap(mCanvasBitmap);
+
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+
+        if (mIsReadOnly) {
+            return false;
+        }
+
         float touchX = event.getX();
         float touchY = event.getY();
 
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
-                drawPath.moveTo(touchX, touchY);
+                mDrawPath.moveTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_MOVE:
-                drawPath.lineTo(touchX, touchY);
+                mDrawPath.lineTo(touchX, touchY);
                 break;
             case MotionEvent.ACTION_UP:
-                drawCanvas.drawPath(drawPath, drawPaint);
-                drawPath.reset();
+                mDrawCanvas.drawPath(mDrawPath, mDrawPaint);
+                mDrawPath.reset();
                 break;
             default:
                 return false;
         }
         invalidate();
         return true;
+
     }
 
-/* Method */
+    /* Method */
 
-    private void setupDrawing(){
-        drawPath = new Path();
-        drawPaint = new Paint();
+    private void setupDrawing() {
+        mDrawPath = new Path();
+        mDrawPaint = new Paint();
 
-        drawPaint.setColor(paintColor);
-
-
-        drawPaint.setAntiAlias(true);
-        drawPaint.setStrokeWidth(20);
-        drawPaint.setStyle(Paint.Style.STROKE);
-        drawPaint.setStrokeJoin(Paint.Join.ROUND);
-        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+        mDrawPaint.setColor(mPaintColor);
 
 
-        canvasPaint = new Paint(Paint.DITHER_FLAG);
+        mDrawPaint.setAntiAlias(true);
+        mDrawPaint.setStrokeWidth(mStrokeWidth);
+        mDrawPaint.setStyle(Paint.Style.STROKE);
+        mDrawPaint.setStrokeJoin(Paint.Join.ROUND);
+        mDrawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        mCanvasPaint = new Paint(Paint.DITHER_FLAG);
+
+
+
     }
+
+    public void clearCanvas() {
+
+        mDrawCanvas.drawColor(mBackgroundColor, PorterDuff.Mode.CLEAR);
+        mCanvasBitmap.eraseColor(mBackgroundColor);
+        invalidate();
+    }
+
+
 }
